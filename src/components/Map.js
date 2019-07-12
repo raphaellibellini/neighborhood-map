11eba96f6fd8cd448937135539a894d30528b437
Raphaelli Bellini <rafael.bellini@hotmail.com>
2019-07-12 19:47:04 -0300

    Change default values of map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
+const defaultCenter = { lat: 45.434479, lng: 12.334806 }
+
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>
   <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    defaultZoom={16}
+    defaultCenter={defaultCenter}
   >
     {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
   </GoogleMap>
